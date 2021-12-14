444d15343de8b25209c9e1530079cdc2be97bbc3
vagrant <wong_michael@rogers.com>
2021-12-14 05:46:16 +0000

    implement the character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,15 @@
 //jQuery
 
 $(document).ready(function() {
-  // --- our code goes here ---
+  console.log("JS Script here");
+
+
+$("#tweet-text").on("input", function(event) {
+  let counter = $(this).val().length;
+  let totalChars = 140;
+  let remainingChars = totalChars - counter;
+
+  $(".counter").val(`${remainingChars}`);
+})
+
 });
