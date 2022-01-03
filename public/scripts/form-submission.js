a90925abf8f589db0573d57b810c76e5c9f4f993
vagrant <wong_michael@rogers.com>
2022-01-03 06:04:55 +0000

    Form submission using JQuery

--- public/scripts/form-submission.js
+++ public/scripts/form-submission.js
@@ -1,16 +1,31 @@
+const { text } = require("body-parser");
+
 $(document).ready(function() {
 
   $(".tweet-form").submit(function(event) {
     event.preventDefault();
 
-    console.log("Form submitted...ajax call")
-  
+    const newTweet = $(this).serialize();
+    const textLength = $(this).serializeArray()["0"].value.length;
+
+    if (textLength === 0) {
+      alert("Please input your tweet for submission!")
+      return;
+    }
+
+    if (textLength >= 140) {
+      alert("Please reduce the number of characters in your Tweet in order to submit! (Max: 140)");
+      return;
+    } 
+
     $.ajax({
       type: "POST",
-      url: "http://localhost:8080/tweets",
-      data: $(this).serialize()
+      url: "/tweets",
+      data: newTweet
     }).then(response => {
       console.log("Successfully posted to the server using Ajax.");
+
+
     });
-});
-});
+  });
+  });
