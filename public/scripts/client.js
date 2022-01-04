3c0bd67c9a14204ff3e5abbac71c9ff0e980911f
vagrant <wong_michael@rogers.com>
2022-01-04 04:37:23 +0000

    Implements ajax get request in client.js and remove driver code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,33 +5,6 @@
  */
 
 $(document).ready(function() {
-  console.log("Test script running!!!");
-
-  const tweetData =
-    [
-      {
-        "user": {
-          "name": "Newton",
-          "avatars": "https://i.imgur.com/73hZDYK.png",
-          "handle": "@SirIsaac"
-        },
-        "content": {
-          "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1641013853984
-      },
-      {
-        "user": {
-          "name": "Descartes",
-          "avatars": "https://i.imgur.com/nlhLi3I.png",
-          "handle": "@rd"
-        },
-        "content": {
-          "text": "Je pense , donc je suis"
-        },
-        "created_at": 1641100253984
-      }
-    ]
 
   function createTweetElement(tweetData) {
 
@@ -76,9 +49,18 @@ $(document).ready(function() {
   function renderTweets(data) {
     for (let obj of data) {
       $(".container").append(createTweetElement(obj));
-    }
-  };
+  }
+}; 
 
-  renderTweets(tweetData);
+function loadTweets() {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/tweets",
+  }).then(response => {
+    renderTweets(response);
+  })
+};
 
-}); 
+loadTweets();
+
+});
